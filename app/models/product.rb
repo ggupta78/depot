3a64a66ba2a620786010c118920a7bdfe7d09fbc
Gautam Gupta <ggupta78@gmail.com>
2024-04-06 13:50:14 +0530

    catalog display done, cart creation doing

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,20 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-  validates :image_url, allow_blank: true,format: {
-    with: %r{\.(gif|jpg|png)\Z}i,
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png)\Z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, 'Line items present')
+    throw :abort
+  end
 end
