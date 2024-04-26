378954150555a26feede08819dcdcfe4a8392ecc
Gautam Gupta <ggupta78@gmail.com>
2024-04-26 16:41:07 +0800

    F2: Turbo partially done..

--- cart_tmp.rb
+++
@@ -1,8 +0,0 @@
-Cart.all.each do |cart|
-  binding.break
-  grouped = cart.line_items.group(:product_id)
-  p grouped.class
-  sums = grouped.sum(:quantity)
-  p sums
-  sums.each { |product_id, quantity| p "#{product_id} - #{quantity}" }
-end
